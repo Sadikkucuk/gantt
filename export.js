da6eeaaa97b0d39de5a582b1c424176edfa98008
Sadik Kucuk <sadik.kucuk3739342@gmail.com>
2025-12-28 12:22:10 +0300

    Update export.js

--- export.js
+++ export.js
@@ -1,17 +1,43 @@
-function exportPNG() {
-  html2canvas(document.getElementById("gantt-wrapper")).then(canvas => {
-    const a = document.createElement("a");
-    a.download = "gantt.png";
-    a.href = canvas.toDataURL();
-    a.click();
-  });
-}
-
 function exportPDF() {
-  html2canvas(document.getElementById("gantt-wrapper")).then(canvas => {
+  const container = document.getElementById("gantt-wrapper");
+
+  html2canvas(container, {
+    scale: 2,
+    useCORS: true
+  }).then(canvas => {
     const { jsPDF } = window.jspdf;
-    const pdf = new jsPDF("l", "px", [canvas.width, canvas.height]);
-    pdf.addImage(canvas, "PNG", 0, 0);
-    pdf.save("gantt.pdf");
+
+    const imgData = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF("l", "pt", "a4");
+
+    const pageWidth = pdf.internal.pageSize.getWidth();
+    const pageHeight = pdf.internal.pageSize.getHeight();
+
+    const imgWidth = canvas.width;
+    const imgHeight = canvas.height;
+
+    // AUTO FIT SCALE
+    const scale = Math.min(
+      pageWidth / imgWidth,
+      pageHeight / imgHeight
+    );
+
+    const renderWidth = imgWidth * scale;
+    const renderHeight = imgHeight * scale;
+
+    const xOffset = (pageWidth - renderWidth) / 2;
+    const yOffset = (pageHeight - renderHeight) / 2;
+
+    pdf.addImage(
+      imgData,
+      "PNG",
+      xOffset,
+      yOffset,
+      renderWidth,
+      renderHeight
+    );
+
+    pdf.save("gantt-autofit.pdf");
   });
 }
